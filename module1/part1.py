e2b28fbac06cd6640ae5c7e64fa2dbd00c1acc3d
Richard Bogart <trbogart@gmail.com>
2025-01-16 20:54:58 -0800

    error handling

--- module1/part1.py
+++ module1/part1.py
@@ -1,6 +1,13 @@
+def read_int(args):
+    while True:
+        try:
+            return int(input(args))
+        except ValueError:
+            print('Enter valid integer')
+
 def main():
-    num1 = int(input('Enter num1: '))
-    num2 = int(input('Enter num2: '))
+    num1 = read_int('Enter num1: ')
+    num2 = read_int('Enter num2: ')
     print(f'Sum is {num1 + num2}')
     print(f'Difference is {num1 - num2}')
 
