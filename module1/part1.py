9ba4bd9d8488a00cc99f287edd69a5a2a796b860
Richard Bogart <trbogart@gmail.com>
2025-01-16 20:30:32 -0800

    Module 1

---
+++ module1/part1.py
@@ -0,0 +1,8 @@
+def main():
+    num1 = int(input('Enter num1: '))
+    num2 = int(input('Enter num2: '))
+    print(f'Sum is {num1 + num2}')
+    print(f'Difference is {num1 - num2}')
+
+if __name__ == '__main__':
+    main()
---
+++ module1/part2.py
@@ -0,0 +1,8 @@
+def main():
+    num1 = int(input('Enter num1: '))
+    num2 = int(input('Enter num2: '))
+    print(f'Product is {num1 * num2}')
+    print(f'Quotient is {num1 / num2}')
+
+if __name__ == '__main__':
+    main()
