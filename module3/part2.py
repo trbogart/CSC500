6e4b2f50c845dde78c6e3c220077826db7b6b6f3
Richard Bogart <trbogart@gmail.com>
2025-01-28 19:24:40 -0800

    module 3

--- module3/part2.py
+++ module3/part2.py
@@ -1,17 +1,12 @@
-def get_hours(s):
-    while True:
-        try:
-            return int(input(s))
-        except ValueError:
-            pass # repeat prompt
-
 def main():
     # get current time in hours (24 hour time)
-    current_hour = get_hours('Enter current hour of the day using 24 hour time: ')
+    current_hour = int(input('Enter current hour of the day using 24 hour time: '))
     # get wait time
-    wait_hours = get_hours('Enter wait time in hours: ')
+    wait_hours = int(input('Enter wait time in hours: '))
+    # add times together and take the modulus of 24
     alarm_time = (current_hour + wait_hours) % 24
     if alarm_time < 0:
+        # adjust for negative numbers
         alarm_time += 24
     print(f'The alarm will go off at {alarm_time}.')
 
