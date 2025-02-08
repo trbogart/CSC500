fa8d00ea857b7061dc28c440c8e92b562709cde2
Richard Bogart <trbogart@gmail.com>
2025-02-08 14:53:31 -0800

    docs

--- module4/ItemToPurchase2.py
+++ module4/ItemToPurchase2.py
@@ -1,5 +1,5 @@
 """
-An item to purchase.
+Item to purchase.
 Attributes:
 - item_name: item name (string, default "none")
 - item_price: item price (float, default 0.0)
@@ -20,19 +20,36 @@ class ItemToPurchase:
         self.item_price = item_price
         self.item_quantity = item_quantity
 
-    """Return the human-readable string representation including total cost, e.g. "Bottled Water 10 @ $1 = $10."""
+    """
+    String representation including total cost, e.g. "Bottled Water 10 @ $1 = $10"
+    """
     def __str__(self):
         return f'{self.item_name} {self.item_quantity} @ ${self.item_price:.2f} = ${self.get_item_cost():.2f}'
 
-    """Return the total cost as a float (item_price * item_quantity) """
+    def __repr__(self):
+        return f"ItemsToPurchase(item_name={self.item_name}, item_price={self.item_price}, item_quantity={self.item_quantity})"
+
+    def __eq__(self, other):
+        return self.item_name == other.item_name and self.item_quantity == other.item_quantity and self.item_price == other.item_price
+
+    def __hash__(self):
+        return hash((self.item_name, self.item_quantity, self.item_price))
+
+    """
+    Returns total cost as a float (item_price * item_quantity) 
+    """
     def get_item_cost(self):
         return self.item_price * self.item_quantity
 
-    """Print the string representation."""
+    """
+    Print string representation. 
+    """
     def print_item_cost(self):
         print(self) # see __str__
 
-"""Prints a list of items including the total cost."""
+"""
+Prints a list of items including the total cost.
+"""
 def print_total_cost(*items):
     total_cost = 0.0
     for item in items:
@@ -40,7 +57,9 @@ def print_total_cost(*items):
         total_cost += item.get_item_cost()
     print(f"Total: ${total_cost:.2f}")
 
-"""Prompts the user to input an item to purchase."""
+"""
+Prompts the user to input an item to purchase.
+"""
 def input_item():
     item = ItemToPurchase()
     item.item_name = input("Enter the item name: ")
@@ -48,11 +67,16 @@ def input_item():
     item.item_quantity = int(input("Enter the item quantity: "))
     return item
 
-"""Prompt user for two items and create two objects of the ItemToPurchase class."""
-if __name__ == "__main__":
+"""
+Prompt user for two items and create two objects of the ItemToPurchase class.
+"""
+def main():
     print("Item 1")
     item1 = input_item()
     print("Item 2")
     item2 = input_item()
     print("TOTAL COST")
     print_total_cost(item1, item2)
+
+if __name__ == "__main__":
+    main()
